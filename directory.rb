cfde3cca6fc8a2bde120650444e85ef26d1344c2
Josh Hemsley <josh@breathehr.com>
2019-06-10 13:37:18 +0100

    Exercise 1 - added each.with_index displaying number of each student

--- directory.rb
+++ directory.rb
@@ -12,20 +12,22 @@ def input_students
   end
   students
 end
+
 def print_header
   puts "The students of the Villians Academy"
   puts "-------------"
 end
+
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+  students.each.with_index(1) { |student , index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)" }
 end
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students 
+students = input_students
 print_header
 print(students)
 print_footer(students)
